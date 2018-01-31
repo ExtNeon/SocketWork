f0b21ac41ef2faed31caa254b2233e5400b1f361
Кирилл Малякин <malyakin-10@yandex.ru>
2018-01-31 03:03:57 +0300

    Написаны комментарии, добавлена возможность чтения буфера не через обработчик.

--- src/hardcore/chat/utils/handledServer/threaded_classes/ThreadedHandledConnection.java
+++ src/hardcore/chat/utils/handledServer/threaded_classes/ThreadedHandledConnection.java
@@ -8,13 +8,25 @@ import java.net.Socket;
 import java.net.SocketException;
 
 /**
- * Created by Кирилл on 25.01.2018.
+ * Данный класс представляет собой одно сокетное соединение, работа с которым происходит в отдельном потоке.
+ * Отличается от стандартной реализации не только многопоточностью, но и возможностью подключения обработчиков.
+ * <ol>
+ *     <li>Обработчик событий подключения и отключения сокета</li>
+ *     <li>Обработчик получения данных по определённому символу</li>
+ * </ol>
+ * Обработчик подключения и отключения сокета используется в клиентской реализации. Он указывается при создании объекта.
+ * Обработчик получения данных используется как в клиентской, так и в серверной реализации. Его суть в том,
+ * что при получении данных, они накапливаются в буфере до тех пор, пока не будет обнаружен символ прерывания.
+ * Как только это произошло, вызывается обработчик получения данных, а в параметры ему передаются полученные данные.
+ * Все обработчики вызываются в отдельных потоках.
+ * @author Малякин Кирилл, гр. 15ИТ20.
  */
 public class ThreadedHandledConnection extends Thread implements Closeable {
     private Socket socket;
     private InterruptableByData dataHandler = null;
     private InterruptableByConnection connectionHandler;
     private char interruptChar = '\0';
+    private StringBuilder receiveStrBuf = new StringBuilder();
 
     public ThreadedHandledConnection(Socket clientSocket, InterruptableByConnection connectionHandler) {
         socket = clientSocket;
@@ -31,14 +43,13 @@ public class ThreadedHandledConnection extends Thread implements Closeable {
         try (InputStream inputStream = socket.getInputStream()) {
             int readedInt;
             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-            StringBuilder gettedStr = new StringBuilder();
             connectionHandler.onNewConnection(this);
             while (!isInterrupted()) {
                 while ((readedInt = reader.read()) != -1) {
-                    gettedStr.append((char) readedInt);
+                    receiveStrBuf.append((char) readedInt);
                     if (readedInt == interruptChar) {
-                        dataHandler.onReceiveDataFromSocket(gettedStr.toString(), this);
-                        gettedStr.delete(0, gettedStr.length()); //Очищаем строку.
+                        dataHandler.onReceiveDataFromSocket(receiveStrBuf.toString(), this);
+                        clearReceiveBuf(); //Очищаем строку.
                     }
                 }
             }
@@ -58,6 +69,23 @@ public class ThreadedHandledConnection extends Thread implements Closeable {
 
     public Socket getSocket() {
         return socket;
+    }
+
+    /**
+     * Возвращает содержимое буфера получения данных в виде строки.
+     * @param clear Если истина, то буфер очищается.
+     * @return Содержимое буфера приёма данных в виде строки.
+     */
+    public String getReceiveBufContent(boolean clear) {
+        String bufContent = receiveStrBuf.toString();
+        if (clear) {
+            clearReceiveBuf();
+        }
+        return bufContent;
+    }
+
+    private void clearReceiveBuf() {
+        receiveStrBuf.delete(0, receiveStrBuf.length());
     }
 
     @Override
